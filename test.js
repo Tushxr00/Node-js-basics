98d8fe303f61ee7d6060ee6e97c77e01b23bf5d4
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-07-05 18:06:33 +0530

    FEAT: checking the use cases of global varibale in node js and difference with window variable

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
-const name = "mario";
-console.log(name);
+// const name = "mario";
+// console.log(name);
+
+console.log(global);
+setTimeout(() => {
+  console.log("here inside the setTimeout");
+  clearInterval(init);
+}, 3000);
+
+const init = setInterval(() => {
+  console.log("here inside the setInterval");
+}, 1000);
