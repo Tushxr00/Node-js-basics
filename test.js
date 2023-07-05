dd3dcd178364d6d264ef6ede555247f96b913283
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-07-05 18:07:16 +0530

    FEAT: finding the directory name and the file name

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 // const name = "mario";
 // console.log(name);
 
-console.log(global);
-setTimeout(() => {
-  console.log("here inside the setTimeout");
-  clearInterval(init);
-}, 3000);
+// console.log(global);
+// setTimeout(() => {
+//   console.log("here inside the setTimeout");
+//   clearInterval(init);
+// }, 3000);
 
-const init = setInterval(() => {
-  console.log("here inside the setInterval");
-}, 1000);
+// const init = setInterval(() => {
+//   console.log("here inside the setInterval");
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
