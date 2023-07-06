ec05cdf7e3b469d389e1fc74201c6d13cdcabff7
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-07-06 10:02:44 +0530

    FEAT: finding the os and getting the platform and homedir

--- modules.js
+++ modules.js
@@ -1,3 +1,7 @@
 const { people, ages } = require("./people");
 
-console.log(people, ages);
+// console.log(people, ages);
+
+const os = require("os");
+
+console.log({ platform: os.platform(), homedir: os.homedir() });
