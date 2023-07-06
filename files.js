dec71652f8ef048a6233ff4f5804cb2d3f5bf630
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-07-06 10:13:02 +0530

    FEAT: writing  a file using node

--- files.js
+++ files.js
@@ -4,20 +4,24 @@ const fs = require("fs");
  * reading files
  * */
 
-console.log("are you waiting for me senpai");
+// console.log("are you waiting for me senpai");
 
-fs.readFile("./docs/blog1.txt", (err, data) => {
-  if (err) {
-    console.log({ err });
-  }
-  console.log({ data: data.toString() });
-});
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) {
+//     console.log({ err });
+//   }
+//   console.log({ data: data.toString() });
+// });
 
-console.log("Senpai ?");
+// console.log("Senpai ?");
 
 /**
  * writing files
  * */
+
+fs.writeFile("./docs/blog1.txt", "hello, senpai", (err, data) => {
+  console.log("file was written");
+});
 
 /**
  * diretories
