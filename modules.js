723ca4b3e4dcd0ba3f08a8c4e6923ff320724d0e
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-12-30 22:20:04 +0530

    FEAT: removed exisiting code

--- modules.js
+++
@@ -1,7 +0,0 @@
-const { people, ages } = require("./people");
-
-// console.log(people, ages);
-
-const os = require("os");
-
-console.log({ platform: os.platform(), homedir: os.homedir() });
--- people.js
+++
@@ -1,5 +0,0 @@
-const people = ["John", "Bruce", "David", "Damien", "Stefan"];
-const ages = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
-// console.log({ people });
-
-module.exports = { people, ages };
