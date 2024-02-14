8d293366b74a3f2dc66787a07746037a6efa5e47
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2024-02-14 11:11:01 +0530

    FEAT: added types for node js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import http from core global modules of nodejs
 const http = require("http");
 const fs = require("fs");
+
 const rqListener = (request, response) => {
   const url = request.url;
   const method = request.method;
@@ -41,4 +42,4 @@ const rqListener = (request, response) => {
 const server = http.createServer(rqListener);
 
 // node js starts to listen and its listening the port 3000 as we have passed as props
-server.listen(3000);
+server.listen(3001);
