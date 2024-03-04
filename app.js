9d5a2175ae364e7874244960bb29b8eceaeae045
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2024-03-04 15:21:09 +0530

    FEAT: split code in different files for readablity

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
 // import http from core global modules of nodejs
 const http = require("http");
-const fs = require("fs");
+const requestHandler = require("./routes");
 
-const rqListener = (request, response) => {
-  const url = request.url;
-  const method = request.method;
-  if (url === "/") {
-    response.write("<html>");
-    response.write("<head><title>Enter Message</title></head>");
-    response.write(
-      "<body><form action='/message' method='POST'> <input type='text' name='message' ><button type='submit'>Send Info</button></form></body>"
-    );
-    response.write("</html>");
-    return response.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    request.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      fs.writeFile("message.txt", parsedBody.split("=")[1], (err) => {
-        response.statusCode = 302;
-        response.setHeader("Location", "/");
-        return response.end();
-      });
-    });
-  }
-  response.setHeader("Content-Type", "text/html");
-  response.write("<html>");
-  response.write("<head><title>my first page</title></head>");
-  response.write(
-    "<body><h1> This is an hello world response form my node js application</h1></body>"
-  );
-  response.write("</html>");
-  response.end();
-};
+// const rqListener = (request, response) => {
+// };
 
 // creating a server using http.createServer merthod
 // rqListener will always run when we send a request to the server
-const server = http.createServer(rqListener);
+const server = http.createServer(requestHandler);
 
 // node js starts to listen and its listening the port 3000 as we have passed as props
 server.listen(3001);
---
+++ routes.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const requestHandler = (request, response) => {
+  const url = request.url;
+  const method = request.method;
+  if (url === "/") {
+    response.write("<html>");
+    response.write("<head><title>Enter Message</title></head>");
+    response.write(
+      "<body><form action='/message' method='POST'> <input type='text' name='message' ><button type='submit'>Send Info</button></form></body>"
+    );
+    response.write("</html>");
+    return response.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    request.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      fs.writeFile("message.txt", parsedBody.split("=")[1], (err) => {
+        response.statusCode = 302;
+        response.setHeader("Location", "/");
+        return response.end();
+      });
+    });
+  }
+  response.setHeader("Content-Type", "text/html");
+  response.write("<html>");
+  response.write("<head><title>my first page</title></head>");
+  response.write(
+    "<body><h1> This is an hello world response form my node js application</h1></body>"
+  );
+  response.write("</html>");
+  response.end();
+};
+
+module.exports = requestHandler;
